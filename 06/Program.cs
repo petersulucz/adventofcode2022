c1fc0d27042d0526c5f0277ed0524504d0f0dd56
Peter Sulucz <peter.sulucz@hotmail.com>
2022-12-05 22:01:36 -0800

    Numba 6

---
+++ 06/Program.cs
@@ -0,0 +1,25 @@
+﻿
+public static class Program
+{
+
+    private static int FindStartSequence(ReadOnlyMemory<char> input, int len) => 
+        Enumerable.Range(len, input.Length - len)
+            .First(idx => input.Slice(idx - len, len).ToArray().Distinct().Count() == len);
+
+    public static int Main(string[] args)
+    {
+        using var file = File.OpenRead(args[0]);
+        using var fileReader = new StreamReader(file);
+
+        string? line;
+        while ((line = fileReader.ReadLine()) != null)
+        {
+            var mem = line.AsMemory();
+            var packet = FindStartSequence(mem, 4);
+            Console.WriteLine("Start packet: " + packet);
+            Console.WriteLine("Start Message: " + (FindStartSequence(mem.Slice(packet), 14) + packet));
+        }
+
+        return 0;
+    }
+}
